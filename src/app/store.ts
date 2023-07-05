f24efa662d6c6e97ed2f27f441c5ab47aea94b86
blank <85779539+blank1729@users.noreply.github.com>
2023-07-05 22:54:52 +0530

    updated gitignore file

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import { atom } from "jotai";
 
-const audio = new Audio("/audio/life-is-fun.mp3");
-// const audio = new Audio("https://www.radioking.com/play/feba-online");
+// const audio = new Audio("/audio/life-is-fun.mp3");
+const audio = new Audio("https://www.radioking.com/play/feba-online");
 audio.preload = "metadata";
 
 export const playerAtom = atom(false);
